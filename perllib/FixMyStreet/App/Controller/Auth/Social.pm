5b73ac713d31954eef4f8b901fdc129004552885
Dave Arter <davea@mysociety.org>
2020-05-13 16:42:04 +0100

    Include redirect URI when fetching OIDC access token
    WIP - this may not always be appropriate

--- perllib/FixMyStreet/App/Controller/Auth/Social.pm
+++ perllib/FixMyStreet/App/Controller/Auth/Social.pm
@@ -281,6 +281,7 @@ sub oidc_callback: Path('/auth/OIDC') : Args(0) {
     eval {
         $id_token = $oidc->get_access_token(
             code => $c->get_param('code'),
+            redirect_uri => $c->uri_for('/auth/OIDC')
         );
     };
     if ($@) {
